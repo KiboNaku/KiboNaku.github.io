8032d67ff17fe4dc1625c8ed73d72770dac86fd2
Andy Ni <nakukibo@gmail.com>
2019-06-18 22:23:34 -0500

    enhanced css styling

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,12 @@
-window.addEventListener("mousemove", function(e){
-    
-});
+
+function size_main(){
+    var main_width = document.getElementById("jump_sidebar").parentElement.offsetWidth -
+                    document.getElementById("jump_sidebar").offsetWidth;
+    document.getElementById("main_body").style.width = main_width + "px"; 
+}
+
+size_main();
+
+window.onresize = function(event) {
+    size_main();
+};
