63d8b7b647586391dd7adc635e567d9fe2f4b30a
Andy Ni <nakukibo@gmail.com>
2020-09-29 11:27:21 -0500

    Updates

---
+++ precache-manifest.c072eb1f7a807c63afb577550f6ce80b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9c38841bc98b4ec6114ed433898b6b1",
+    "url": "/KiboNaku.github.io/index.html"
+  },
+  {
+    "revision": "1cc7cea0cc69401e0cd7",
+    "url": "/KiboNaku.github.io/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "bfff395624971933275d",
+    "url": "/KiboNaku.github.io/static/css/main.b4fe1218.chunk.css"
+  },
+  {
+    "revision": "1cc7cea0cc69401e0cd7",
+    "url": "/KiboNaku.github.io/static/js/2.3ba3d831.chunk.js"
+  },
+  {
+    "revision": "48708d0bba57d355d4ffc14a8bfac5cf",
+    "url": "/KiboNaku.github.io/static/js/2.3ba3d831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfff395624971933275d",
+    "url": "/KiboNaku.github.io/static/js/main.36c9cdcd.chunk.js"
+  },
+  {
+    "revision": "f9d04fad4961e8819b2e",
+    "url": "/KiboNaku.github.io/static/js/runtime-main.c48b5e90.js"
+  }
+]);
