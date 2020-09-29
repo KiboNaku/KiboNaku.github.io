6be4269716ab556b91668495ad2050f8f63bd8aa
Andy Ni <nakukibo@gmail.com>
2020-09-29 11:00:25 -0500

    initialized landing

---
+++ src/components/Landing.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import "./Landing.css"
+
+class Landing extends Component {
+
+    render() {
+
+        return (
+            <main id="landing-main">
+                <div >
+                    
+                    <p>
+                        Hello, my name is
+                    </p>
+                        
+                    <p>
+                        Andy Ni
+                    </p>
+
+                    <a>Contact me</a>
+                </div>
+            </main>
+        )
+    }
+}
+
+export default Landing
